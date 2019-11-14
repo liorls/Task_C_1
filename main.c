0953a0cb596c24781adfe7ab7d6a11c634d16eb9
liorls <liorsamuel@gmail.com>
2019-11-14 15:24:37 +0200

    all main

--- main.c
+++ main.c
@@ -10,15 +10,15 @@ scanf("%lf",&input);
 
 //f(x) = e^x + x^3 − 2
 double f1 = sub(add(Exp(input),Pow(input,3)),2);
-printf("The value of f(x) = e^x + x^3 − 2 at the point %.4f is: %.4f",input,f1);
+printf("The value of f(x) = e^x + x^3 − 2 at the point %.4f is: %.4f \n",input,f1);
 
 //f(x) = 3x + 2X^2
 double f2 = add(mul(3,(double)input),Pow(mul(2,(double)input),2));
-printf("The value of f(x) = 3x + 2X^2 at the point %.4f is: %.4f",input,f2);
+printf("The value of f(x) = 3x + 2X^2 at the point %.4f is: %.4f \n",input,f2);
 
 //f(x) = (4x^3)/5 -2x 
 double f3 = sub(div(Pow(mul(4,(double)input),3),5),mul(2,(double)input));
-printf("The value of f(x) = (4x^3)/5 -2x at the point %.4f is: %.4f",input,f3);
+printf("The value of f(x) = (4x^3)/5 -2x at the point %.4f is: %.4f \n",input,f3);
 
 return 0;
 }
