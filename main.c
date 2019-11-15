9c5551093cd330356be4df51e42e5c8f531f1987
liorls <57623323+liorls@users.noreply.github.com>
2019-11-15 12:39:36 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
 #include "myMath.h"
 
-int main(int argc, char const *argv[]){
+int main(int argc, char const *argv[])
+{
 
 double input;
 printf("Please inset a real number: ");
 scanf("%lf",&input);
 
 //f(x) = e^x + x^3 − 2
-double f1 = sub(add(Exp(input),Pow(input,3)),2);
-printf("The value of f(x) = e^x + x^3 − 2 at the point %.4f is: %.4f \n",input,f1);
+
+float f1 = sub(add((float)Exp((int)input),(float)Pow(input,3)),(float)2);
+printf("The value of f(x) = e^x + x^3 − 2 at the point %.4f is: %.4f",input,f1);
 
 //f(x) = 3x + 2X^2
-double f2 = add(mul(3,(double)input),Pow(mul(2,(double)input),2));
-printf("The value of f(x) = 3x + 2X^2 at the point %.4f is: %.4f \n",input,f2);
+float f2 = add((float)mul(input,3),(float)Pow(mul(input,2),2));
+printf("The value of f(x) = 3x + 2X^2 at the point %.4f is: %.4f",input,f2);
 
 //f(x) = (4x^3)/5 -2x 
-double f3 = sub(div(Pow(mul(4,(double)input),3),5),mul(2,(double)input));
-printf("The value of f(x) = (4x^3)/5 -2x at the point %.4f is: %.4f \n",input,f3);
+float f3 = sub((float)div(Pow(mul(input,4),3),5),(float)mul(input,2));
+printf("The value of f(x) = (4x^3)/5 -2x at the point %.4f is: %.4f",input,f3);
 
 return 0;
 }
